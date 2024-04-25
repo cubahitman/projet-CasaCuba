a37eb68908c690491205a9d6d8e9ee1e4df0bca5
Roberto <rquesadarad@yahoo.fr>
2024-04-25 12:55:53 +0200

    commit save

--- index.php
+++ index.php
@@ -26,11 +26,11 @@ require_once "inc/header.inc.php";
                 </li>
                 <li class='item' style="background-image: url('assets/img/playa.jpg')">
                     <div class='content'>
-                        <h2 class='title'>"Estrange Bond"</h2>
+                        <h2 class='title'> Un paradis tropical aux multiples facettes</h2>
                         <p class='description'> Lorem ipsum, dolor sit amet consectetur
                             adipisicing elit. Tempore fuga voluptatum, iure corporis inventore
                             praesentium nisi. Id laboriosam ipsam enim. </p>
-                        <button>Read More</button>
+                        <button>Voire plus</button>
                     </div>
                 </li>
                 <li class='item' style="background-image: url('assets/img/espectaculo-en-el-cabaret.jpg')">
@@ -39,25 +39,25 @@ require_once "inc/header.inc.php";
                         <p class='description'> Lorem ipsum, dolor sit amet consectetur
                             adipisicing elit. Tempore fuga voluptatum, iure corporis inventore
                             praesentium nisi. Id laboriosam ipsam enim. </p>
-                        <button>Read More</button>
+                        <button>Voire plus</button>
                     </div>
                 </li>
                 <li class='item' style="background-image: url('assets/img/cienfuegos.jpg')">
                     <div class='content'>
-                        <h2 class='title'>"Last Trace Of Us"</h2>
+                        <h2 class='title'>"CIENFUEGOS"</h2>
                         <p class='description'>
                             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore fuga voluptatum, iure corporis inventore praesentium nisi. Id laboriosam ipsam enim.
                         </p>
-                        <button>Read More</button>
+                        <button>Voire plus</button>
                     </div>
                 </li>
                 <li class='item' style="background-image: url('assets/img/istockphoto-1093639450-612x612.jpg')">
                     <div class='content'>
-                        <h2 class='title'>"Urban Decay"</h2>
+                        <h2 class='title'>"VARADERO"</h2>
                         <p class='description'>
                             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore fuga voluptatum, iure corporis inventore praesentium nisi. Id laboriosam ipsam enim.
                         </p>
-                        <button>Read More</button>
+                        <button>Voire plus</button>
                     </div>
                 </li>
                 <li class='item' style="background-image: url('assets/img/toomas-tartes-ME72-VPJH2I-unsplash.jpg')">
@@ -66,7 +66,7 @@ require_once "inc/header.inc.php";
                         <p class='description'> Lorem ipsum, dolor sit amet consectetur
                             adipisicing elit. Tempore fuga voluptatum, iure corporis inventore
                             praesentium nisi. Id laboriosam ipsam enim. </p>
-                        <button>Read More</button>
+                        <button>Voire plus</button>
                     </div>
                 </li>
             </ul>
@@ -85,12 +85,12 @@ require_once "inc/header.inc.php";
             <div class="card mb-3 " style="max-width: 540px;">
                 <div class="row g-0">
                     <div class="col-md-4  md-mt-5">
-                        <img src="assets/img/premium_photo-1661859256790-5ead8d1a3d6f.avif" class="img-fluid rounded-start " alt="image couple contemple paysage">
+                        <img src="assets/img/premium_photo-1661859256790-5ead8d1a3d6f.avif" class="img-fluid rounded-start h-100 " alt="image couple contemple paysage">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. </p>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa </p>
 
                         </div>
                     </div>
@@ -100,6 +100,15 @@ require_once "inc/header.inc.php";
     </section>
 
     <section class="populars ">
+        <div class="mx-5">
+            <h3 class=" emplacement">Populaires</h3>
+            <div class="row mx-auto text-center">
+                <div class="col-3 "><a href="#"><img src="assets/img/femmeBalance.png" class="rounded-4" alt="Femme dans une balançoire à la plage.">image</a></div>
+                <div class="col-3"><a href="#"><img src="assets/img/havane.png" class="rounded-4" alt="image de la havane">image</a><a href="#"><img src="assets/img/santiago.jpeg" class="rounded-4" alt="image de santiago"></a></div>
+                <div class="col-3"><a href="#"><img src="assets/img/vinallesLink.jpeg" class="rounded-4" alt="image de viñales">image</a></a></div>
+                <div class="col-3"><a href="#"><img src="assets/img/trinidadLink.jpeg" class="rounded-4" alt="image de trinidad Cuba">image</a><img src="assets/img/cienfuegosLink.jpeg" class="rounded-4" </div>
+
+                </div>
 
     </section>
 </main>
